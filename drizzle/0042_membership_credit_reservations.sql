46a23a5a5cc0eb39ca0950e56a01535ffdc8179f
kaylonestefani2016-del <kaylon.estefani2016@gmail.com>
2026-09-20 13:39:49 -0300

    Evitar consolidar vínculos legados inconsistentes na migração

--- drizzle/0042_membership_credit_reservations.sql
+++ drizzle/0042_membership_credit_reservations.sql
@@ -3,16 +3,6 @@ ALTER TABLE `appointments` ADD `membership_plan_id` integer;
 ALTER TABLE `appointments` ADD `membership_credit_state` text;
 ALTER TABLE `daily_records` ADD `appointment_id` integer;
 
-UPDATE `plans`
-SET `service_id` = (
-  SELECT `services`.`id`
-  FROM `services`
-  WHERE `services`.`organization_id` = `plans`.`organization_id`
-    AND `services`.`deleted_at` IS NULL
-    AND lower(trim(`services`.`name`)) = lower(trim(`plans`.`plan_kind`))
-  LIMIT 1
-)
-WHERE `service_id` IS NULL;
 
 CREATE UNIQUE INDEX IF NOT EXISTS `daily_records_appointment_unique`
 ON `daily_records` (`appointment_id`);
